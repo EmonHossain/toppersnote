758c1dd8f3838287310857b55d94913dfe3a6805
Emon <emondipu@gmail.com>
2026-06-15 10:59:30 +0200

    set global loging properties.

--- src/main/java/com/sharenote/ShareNoteApplication.java
+++ src/main/java/com/sharenote/ShareNoteApplication.java
@@ -1,11 +1,14 @@
 package com.sharenote;
 
+import com.sharenote.logging.LoggingProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableConfigurationProperties(LoggingProperties.class)
 public class ShareNoteApplication {
 
     public static void main(String[] args) {
