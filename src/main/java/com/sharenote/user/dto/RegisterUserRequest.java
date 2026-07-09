4b3caca9bd56f1c76a2ef7afab68437bb393cc68
Emon <emondipu@gmail.com>
2026-07-09 23:46:02 +0200

    reduced to minimum required fields

--- src/main/java/com/sharenote/user/dto/RegisterUserRequest.java
+++ src/main/java/com/sharenote/user/dto/RegisterUserRequest.java
@@ -10,9 +10,6 @@ public record RegisterUserRequest(
         @Size(max = 100, message = "First name must be at most 100 characters")
         String firstName,
 
-        @Size(max = 100, message = "Middle name must be at most 100 characters")
-        String middleName,
-
         @NotBlank(message = "Last name is required")
         @Size(max = 100, message = "Last name must be at most 100 characters")
         String lastName,
@@ -22,41 +19,11 @@ public record RegisterUserRequest(
         @Size(max = 320, message = "Email must be at most 320 characters")
         String email,
 
-        @NotBlank(message = "Password is required")
-        @Size(min = 8, max = 128, message = "Password must be between 8 and 128 characters")
+        @NotBlank(message = "Password cannot be blank")
+        @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters long")
         @Pattern(
-                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$",
-                message = "Password must include uppercase, lowercase, and number characters"
-        )
-        String password,
-
-        @NotBlank(message = "Institution is required")
-        @Size(max = 120, message = "Institution must be at most 120 characters")
-        String institution,
-
-        @NotBlank(message = "Degree program is required")
-        @Size(max = 120, message = "Degree program must be at most 120 characters")
-        String degreeProgram,
-
-        @NotBlank(message = "Current semester or year is required")
-        @Size(max = 50, message = "Current semester or year must be at most 50 characters")
-        String currentSemesterOrYear,
-
-        @NotBlank(message = "Current year is required")
-        @Size(max = 20, message = "Current year must be at most 20 characters")
-        String currentYear,
-
-        @NotBlank(message = "Current semester is required")
-        @Size(max = 50, message = "Current semester must be at most 50 characters")
-        String currentSemester,
-
-        @NotBlank(message = "Phone number is required")
-        @Size(max = 30, message = "Phone number must be at most 30 characters")
-        @Pattern(regexp = "^\\+?[0-9 .()\\-]{7,30}$", message = "Phone number must be valid")
-        String phoneNumber,
-
-        @NotBlank(message = "Country is required")
-        @Size(max = 100, message = "Country must be at most 100 characters")
-        String country
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\",./<>?|\\\\~`]).+$",
+                message = "Password must include uppercase, lowercase, and either a number or a special character")
+        String password
 ) {
 }
